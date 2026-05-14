e80e5e7ae99d6d932d6eea2c95954677f0039f9f
Haowei Chai <chw930512@gmail.com>
2026-05-14 14:59:01 +0000

    add abnormal week fact model

---
+++ models/marts/finance/fct_carrier_surcharge__abnormal_week_info.sql
@@ -0,0 +1,22 @@
+with invoice as (
+
+    select * from {{ ref('fct_carrier_surcharge__ups_invoice') }}
+
+),
+
+cost as (
+    select * from {{ ref('fct_carrier_surcharge__weekly_cost') }}
+),
+
+abnormal as (
+    select i.invoice_year,
+           i.invoice_week,
+           i.total_amount as invoice_amount,
+           c.total_cost as projection_amount,
+           i.total_amount - c.total_cost as diffrence,
+           concat(round((i.total_amount - c.total_cost) / c.total_cost, 2)*100, '%') as diff_perct
+    from invoice i join cost c on i.invoice_year = c.create_year and i.invoice_week = c.create_week
+    where round((i.total_amount - c.total_cost) / c.total_cost, 2)*100 >= 8
+)
+
+select * from abnormal
